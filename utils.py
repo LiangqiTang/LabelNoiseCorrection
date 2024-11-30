94b9c653be58c0c2750bc3b8d806feeaeaa3add0
LiangqiTang <144564789+LiangqiTang@users.noreply.github.com>
2024-11-30 17:16:02 -0500

    update torch.distributions

--- utils.py
+++ utils.py
@@ -497,9 +497,9 @@ def mixup_data_beta(x, y, B, device='cuda'):
     lam = ((1 - B) + (1 - B[index]))
     mixed_x = ((1-B)/lam).unsqueeze(1).unsqueeze(2).unsqueeze(3) * x + ((1-B[index])/lam).unsqueeze(1).unsqueeze(2).unsqueeze(3) * x[index, :]
     # Here we use a new dynamic mixup method by random sampling from a beta distribution 
-    # p1 = np.exp((1-B)/lam)
-    # p2 = np.exp((1-B[index])/lam)
-    # delta = np.random.beta(32*p1, 32*p2)
+    # p1 = torch.exp((1-B)/lam)
+    # p2 = torch.exp((1-B[index])/lam)
+    # delta = torch.distributions.beta.Beta(p1, p2).sample()
     # mixed_x = delta.unsqueeze(1).unsqueeze(2).unsqueeze(3) * x + (1-delta).unsqueeze(1).unsqueeze(2).unsqueeze(3) * x[index, :]
     y_a, y_b = y, y[index]
     return mixed_x, y_a, y_b, index
